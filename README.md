b71fc782ab1941f32366f6ebf904e3c905fe8e42
Karl J <karl@techbento.com>
2016-08-15 11:42:26 -0400

    Webmin installation script and first README file.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # devops
 DevOps Public Repository for TechBento, Portland, Maine.  Includes scripts, AWS shortcuts, and useful code. 
+This repository has sample code, scripts, and test code. Provided AS-IS, no warranty. 
---
+++ install_webmin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# As-Is, no warranty. Use at your own risk.  Works only on Linux RedHat/CentOS.
+# ----------------------------------
+# $Id:$ 
+# ----------------------------------
+echo "================================================================="
+echo "Install WebMin management GUI for administering the server."
+sudo mkdir -p /usr/shared/techbento
+sudo chmod 777 -p /usr/shared/techbento
+wget -N  http://prdownloads.sourceforge.net/webadmin/webmin-1.801-1.noarch.rpm -P /usr/shared/techbento
+sudo yum -y install perl perl-Net-SSLeay openssl perl-IO-Tty
+sudo rpm -U /home/ec2-user/webmin-1.801-1.noarch.rpm
+echo "Please specify WebMin root password:"
+read varWebMinPassword
+/usr/libexec/webmin/changepass.pl /etc/webmin root $varWebMinPassword
+echo "Done. Be sure to update the console." 
